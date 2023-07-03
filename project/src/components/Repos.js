d03c112d87b934c78fe3600ee29064c3108e068c
smajaas <smajaas@gmail.com>
2023-07-03 19:17:25 +0530

    total using reduce and sort

--- project/src/components/Repos.js
+++ project/src/components/Repos.js
@@ -6,6 +6,26 @@ const Repos = () => {
   const { repos } = React.useContext(GithubContext);
   console.log(repos);
 
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
   const chartData = [
     {
       label: 'HTML',
@@ -28,7 +48,7 @@ const Repos = () => {
     <section className='section'>
       <Wrapper className='section-center'>
         {/* <ExampleChart data={chartData} /> */}
-        <Pie3D data={chartData} />
+        <Pie3D data={languages} />
       </Wrapper>
     </section>
   );

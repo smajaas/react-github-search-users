1c6ff00474ce33f30f35d5a15beb07526e191b8a
smajaas <smajaas@gmail.com>
2023-07-03 11:59:45 +0530

    fusion chart

--- project/src/components/Repos.js
+++ project/src/components/Repos.js
@@ -5,7 +5,32 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
   console.log(repos);
-  return <ExampleChart />;
+
+  const chartData = [
+    {
+      label: 'HTML',
+      value: '180',
+    },
+    {
+      label: 'JAVASCRIPT',
+      value: '200',
+    },
+    {
+      label: 'REACT',
+      value: '180',
+    },
+    {
+      label: 'CSS',
+      value: '140',
+    },
+  ];
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <ExampleChart data={chartData} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`

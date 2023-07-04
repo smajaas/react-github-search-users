0671a69888a2ae3c4bbd89518f4db88d4b963e52
smajaas <smajaas@gmail.com>
2023-07-04 08:10:18 +0530

    Login Logout functionality

--- project/src/index.js
+++ project/src/index.js
@@ -8,11 +8,22 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-7w5bo5jhpacnj7oj.us.auth0.com'
+      clientId='BW8MAwoQsne3aLP2aUWjK36xG7Cp7zru'
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
+
+// dev-7w5bo5jhpacnj7oj.us.auth0.com
+
+// BW8MAwoQsne3aLP2aUWjK36xG7Cp7zru;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
